3c05ee68c53bff4a189fcf910858c785eb4d7755
khyeri19 <h.kim2@students.uu.nl>
2023-01-25 10:57:30 +0100

    Lifespan and the number of spouses: R script & image as pdf file

--- Average Lifespan of Royals across centuries.R
+++ Average Lifespan of Royals across centuries.R
@@ -2,9 +2,10 @@ library(tidyverse)
 final_royal <- read_csv('final_royal.csv')
 
 lifespan <- final_royal|>
-  select(title,`ontology/birthYear`,`ontology/deathYear`)|>
+  select(title,`ontology/birthYear`,`ontology/deathYear`,num_spouses)|>
   rename(birthyear = `ontology/birthYear`, deathyear = `ontology/deathYear`)|>
   na.omit()|>
+  filter(num_spouses <= 3)|>
   filter(birthyear >= 0, deathyear >=0)|>
   filter(deathyear > birthyear)
 
@@ -19,12 +20,13 @@ grouped_by_century <- life_mutated |>
   summarise(avg_lifespan = mean(life))
 
 
-#PART3: Plot
+#PART3: Average Plot
 line_plot <- ggplot(data = grouped_by_century)+
   aes (x = century, y = avg_lifespan)+
   geom_line()+
   theme_grey()+
-  ylim(c(0,80))
+  ylim(c(0,80))+
+  xlim(c(9, 20))
 
 
 line_plot <- line_plot + labs (title = 'Average Lifespan of Royals Across Centuries',
@@ -33,4 +35,19 @@ line_plot <- line_plot + labs (title = 'Average Lifespan of Royals Across Centur
 
 print(line_plot)
 
-  
+#PART4: Scatter Plot
+scatter_plot <- ggplot(data = life_mutated)+
+  aes (x = birthyear, y = life, color = as.factor(num_spouses))+
+  geom_point()+
+  theme_grey()+
+  ylim(c(0,110))+
+  xlim(c(1000, 2000)) 
+
+
+scatter_plot <- scatter_plot + labs (title = 'Lifespan of Royals during A.D. 1000-2000',
+                               x = 'Birth Year',
+                               y = 'Life Span',
+                               color = "Number of Spouses")
+
+print(scatter_plot)
+
