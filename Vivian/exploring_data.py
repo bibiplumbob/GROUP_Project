7f6b25a4da4aea08716d53a693b05b262b9e0906
Vivian Pereira <yoonseiyooni@gmail.com>
2023-01-25 15:59:38 +0100

    New CSV with country data

--- Vivian/exploring_data.py
+++ Vivian/exploring_data.py
@@ -34,14 +34,55 @@ for label in royal_data:
     if 'ontology/occupation' in label:
         del label['ontology/occupation']
 
-#Playing around with country set
+#Creating a country set
 with open('country_data.json') as f:
     country = json.load(f)
 
-country_set = set()
+countries = set()
 for dict in country:
     if 'Name' in dict:
-        if dict['Name'] not in country_set:
-            country_set.add(dict['Name'])
+        if dict['Name'] not in countries:
+            countries.add(dict['Name'])
+#Now to try and compare 
+did_not_work = []
+for dict in royal_data:
+    if "ontology/birthPlace_label" in dict:
+        # If only a single place, convert it to a list
+        if type(dict["ontology/birthPlace_label"]) is not list:
+            dict["ontology/birthPlace_label"] = [dict["ontology/birthPlace_label"]]
+        
+        # Convert list of place names to set of placenames (for set intersection later)
+        dict["ontology/birthPlace_label"] = set(dict["ontology/birthPlace_label"])
 
-print(country_set)
+        # Find the set intersection between countries and birthPlace
+        origin_country = dict["ontology/birthPlace_label"] & countries
+
+        #Check that the intersection is just one country
+        if len(origin_country) == 1:
+            #Save single country back into dictionary
+            origin_country = list(origin_country)[0]
+            dict["birthCountry"] = origin_country
+        else:
+            did_not_work.append(dict["ontology/birthPlace_label"])
+
+#Filtering to data that has both birth year and death year 
+birth_death_year = []
+for dict in royal_data:
+    if "ontology/birthYear" in dict:
+        if "ontology/deathYear" in dict:
+            birth_death_year.append(dict)
+
+#Convert string to numbers for the year values
+for dict in birth_death_year:
+    if type(dict["ontology/birthYear"]) is str:
+        dict["ontology/birthYear"] = int(dict["ontology/birthYear"])
+    if type(dict["ontology/deathYear"]) is str:
+        dict["ontology/deathYear"] = int(dict["ontology/deathYear"])
+
+import csv
+columns = ["title","ontology/title","deathPlace","birthPlace","num_spouses","ontology/birthYear","ontology/deathYear","birthCountry"]
+with open('country_royals.csv', 'w', encoding = 'utf-8', newline= '') as f:
+    writer = csv.DictWriter(f, fieldnames= columns, extrasaction='ignore')
+    writer.writeheader()
+    for dict in birth_death_year:
+        writer.writerow(dict)
--- experimental/experimental.py
+++ experimental/experimental.py
@@ -1,19 +1,19 @@
-attempt_dict =[{
-    "words":["hello", "this", "croissant"],
+royals =[{
+    "places":["hello", "this", "croissant"],
     "status": "tired",
     "emotion": ["frustrated", "exhausted"],
     "attempt": "this_is_a_test"
 },
 {
-    "words":["hello", "wow", "carbonara"],
+    "places":["hello", "wow", "carbonara"],
     "status":"hungry",
     "emotion": ["annoyed", "cranky"],
     "attempt": "I_hate_life"
 }]
 
 #Pick last value of a list and add it to a new key
-for dict in attempt_dict:
-    if "words" in dict:
+for dict in royals:
+    if "status" in dict:
         if type(dict["status"]) is list:
             dict["the_word"] = dict["status"][-1]
         else:
@@ -22,7 +22,7 @@ for dict in attempt_dict:
     if "emotion" in dict:
         dict["num_of_emotes"] = len(dict["emotion"])
 #Trying splitting and checking for frequency:
-for dict in attempt_dict:
+for dict in royals:
     if "attempt" in dict:
         split = dict["attempt"].split('_')
     #if "test" or "tests" in split:
@@ -38,15 +38,33 @@ for dict in attempt_dict:
     #    writer.writerow(dict)
 
 #Experimental -- comparing to another set to create a new set
-compare = {"croissant", "carbonara", "coffee"}
+countries = {"croissant", "carbonara", "coffee"}
 manual_group = []
-for dict in attempt_dict:
-    if "words" in dict:
-        for key in dict:
-            if dict["words"] in compare:
-                dict["food_status"] = "my_fave"
-            else:
-                manual_group.append(dict)
+for dict in royals:
+    if "places" in dict:
+        # If only single place, i.e., string: convert to list
+        if not type(dict["places"]) is list:
+            dict["places"] = [dict["places"]]
 
-print(attempt_dict)                
+        # Convert list of place names to set of placenames
+        dict["places"] = set(dict["places"])
+
+        # Find intersection between set of placenames and set of countries
+        origin_countries = dict["places"] & countries
+
+        # Check that intersection only contains single country
+        if len(origin_countries) == 1:
+            # Save single origin country back into dictionary
+            origin_country = list(origin_countries)[0]
+            dict['place'] = origin_country
+        else:
+            print(dict["places"], origin_countries)
+
+        # for dict["words"] in dict:
+        #     if dict["words"] in countries:
+        #         dict["food_status"] = "my_fave"
+        #     else:
+        #         manual_group.append(dict)
+
+print(royals)                
 
