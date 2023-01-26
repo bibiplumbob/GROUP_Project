1db9f3723d73642740bde340e43b074126bd1dbd
Vivian Pereira <yoonseiyooni@gmail.com>
2023-01-26 09:47:00 +0100

    region mapping initial

--- Vivian_Rwork/country_exp_viv.R
+++ Vivian_Rwork/country_exp_viv.R
@@ -10,4 +10,24 @@ filtered_count <- royals_countries|>
   filter(lifespan < 700)|>
   filter(!is.na(birthCountry))
 
-regions = read_csv("region_mapping.csv")
+region_mapping = read_csv("region_mapping.csv")
+
+filtered_count <- left_join(filtered_count, region_mapping, by="birthCountry")
+
+royals_country <- ggplot(data = filtered_count)+
+  aes(x = region, y = lifespan, fill = continent
+      )+
+  geom_bar(stat = "summary",
+           fun = "mean",
+           width = 0.5)+
+  theme(text = element_text(family = 'sans'))+
+  labs(title = "Lifespan across birth regions",
+       subtitle = "A smaller sample size")+
+  xlab("Region of Birth")+
+  ylab("Lifespan (years)")+
+  coord_flip()
+
+print(royals_country)
+
+  
+  
